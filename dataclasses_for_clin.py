43d1d28ac1c13286093ec021010b6c3628fdc0f5
Zeep <denoliinyk44@gmail.com>
2024-04-23 00:33:17 +0200

    fist app 😄

--- dataclasses_for_clin.py
+++ dataclasses_for_clin.py
@@ -1,10 +1,11 @@
 import json
 from dataclasses import dataclass
 
+
 class Studies():
-    protocolSection:list
-    derivedSection:list
-    hasResults:bool
+    protocolSection: list
+    derivedSection: list
+    hasResults: bool
 
     def unpuck(self, file_name):
         try:
@@ -12,41 +13,48 @@ class Studies():
         except Exception as e:
             print(e)
         return file
+
     def unpuck_protocol_section(self, file_name, keyword):
         file = json.load(open(file_name))
         com_data = file.get("studies")
         counter = 0
-       # com_data = file.get("studies")[0].get("protocolSection").get("identificationModule")
+        #com_data = file.get("studies")[2].get("protocolSection").get("identificationModule")
         for element in com_data:
             ps = element.get("protocolSection")
-            im = ps.get("identificationModule")
-            key_get = im.get(keyword)
+            for key_get in ps:
+                key_get = ps.get(keyword)
+            print(key_get)
             counter = counter + 1
 
         print(counter)
         print(key_get)
-@dataclass
-class ProtocolSection():
-    identificationModule:dict
-    statusModule:dict
-    sponsorCollaboratorsModule:dict
-    descriptionModule:dict
-    conditionsModule:dict
-    designModule:dict
-    armsInterventionsModule:dict
-    outcomesModule:dict
-    eligibilityModule:dict
-    contactsLocationsModule:dict
+        #print(com_data)
 
-class IdentificationModule():
-    nctId:str
-    orgStudyIdInfo:dict
-    secondaryIdInfos:list
-    organization:dict
-    briefTitle:str
-    officialTitle:str
+
+@dataclass
+class ProtocolSection(Studies):
+    identificationModule: dict
+    statusModule: dict
+    sponsorCollaboratorsModule: dict
+    descriptionModule: dict
+    conditionsModule: dict
+    designModule: dict
+    armsInterventionsModule: dict
+    outcomesModule: dict
+    eligibilityModule: dict
+    contactsLocationsModule: dict
+
+
+class IdentificationModule(ProtocolSection):
+    nctId: str
+    orgStudyIdInfo: dict
+    secondaryIdInfos: list
+    organization: dict
+    briefTitle: str
+    officialTitle: str
+
 
 if __name__ == "__main__":
     s = Studies()
-    #f = s.unpuck("data.json")
-    s.unpuck_protocol_section("data.json","officialTitle")
+    # f = s.unpuck("data.json")
+    s.unpuck_protocol_section("data.json", "identificationModule")
