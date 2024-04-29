fa118e0f5def23ff9ddfa93e3eead9cd641e7582
ZeeptfSpoon <denoliinyk44@gmail.com>
2024-04-29 19:56:34 +0200

    trying to make this shit work

---
+++ dict exmpl.py
@@ -0,0 +1,19 @@
+import json
+
+
+class Jsonparser_exersise_1():
+    def unpuck(self, file_name, key_word, search_key):
+        d1 = json.load(open(file_name))
+        # print(d1)
+        com_data = d1.get(key_word)
+        print(com_data)
+        for a in com_data:
+            if a == search_key:
+                print(com_data[a])
+            if a != search_key:
+                print("ERROR there is no such key")
+            exit()
+
+if __name__ == "__main__":
+    s = Jsonparser_exersise_1()
+    s.unpuck("dict_for_ex.json", "human", "jso")
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from loguru import logger
 import uvicorn
 import requests
 
+
 class Item(BaseModel):
     name: str
     description: Optional[str] = None
@@ -24,13 +25,13 @@ async def root():
 async def create_item(item: Item):
     return item
 
+
 @app.get("/item/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
 
 @app.get("/users/me", summary="testheader_example")
-
 async def get_users():
     """
     Testing schwager schema
@@ -43,23 +44,25 @@ async def get_users():
 async def read_item(user_id: str, q: Optional[str] = None):
     return {"user_id": user_id, "q": q}
 
+
 @app.get("/get_info/")
 async def get_info():
     reg = requests.get('https://clinicaltrials.gov/api/v2/studies')
     return reg.json()
 
-#@app.get("/page_code")
-#async def get_page_code():
-    #x = requests.get('https://clinicaltrials.gov/api/v2/studies')
-    #reg = x.content
-    #reg = json.loads(x.content)
-    #json_reg = json.dumps(reg, sort_keys=True, indent=4)
-    #return bytes(json_reg,'UTF-8')
-    # reg = requests.get('https://clinicaltrials.gov/')
-    # print(reg.status_code)
+
+# @app.get("/page_code")
+# async def get_page_code():
+# x = requests.get('https://clinicaltrials.gov/api/v2/studies')
+# reg = x.content
+# reg = json.loads(x.content)
+# json_reg = json.dumps(reg, sort_keys=True, indent=4)
+# return bytes(json_reg,'UTF-8')
+# reg = requests.get('https://clinicaltrials.gov/')
+# print(reg.status_code)
 
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8080, reload=False,
                 log_level="debug", workers=1)
-                #limit_concurrency=1, limit_max_requests=1)
+    # limit_concurrency=1, limit_max_requests=1)
