1f3e62d8ade96fcb24e2d3397f9ab8169ad9d4c8
ZeeptfSpoon <denoliinyk44@gmail.com>
2024-07-01 22:04:04 +0200

    trying to make this shit work

--- parser_json_test.py
+++ parser_json_test.py
@@ -8,9 +8,12 @@ class Main:
     """
     studies: list
     nextPageToken: str
+    list_studies: list
 
     def __init__(self, file_name=None):
+        # container list for class obj studies
         file = None
+        self.list_studies = []
         if file_name:
             try:
                 file = json.load(open(file_name))
@@ -22,51 +25,102 @@ class Main:
                 if file.get('studies'):
                     self.studies = file.get('studies')
 
+    def fill_studies(self):
+        for element in self.studies:
+            hasResults = False
+            if element.get("protocolSection"):
+                protocolSection = ProtocolSection(element.get("protocolSection"))
+            if element.get("derivedSection"):
+                derivedSection = element.get("derivedSection")
+            if element.get("hasResults"):
+                hasResults = element.get("hasResults")
+            object_studies = Studies(protocolSection, derivedSection, hasResults)
+            self.list_studies.append(object_studies)
+
+    def fill_protocolSection(self):
+        pass
+
 
 class Studies:
-    protocolSection: dict
+    protocolSection: object
     derivedSection: dict
     hasResults: bool
 
-    def __init__(self, studies_json_as_list: list):
+    def __init__(self, protocolSection, derivedSection, hasResults):
         """
         "protocolSection" dict
-        "derivedSection"dict
+        "derivedSection" dict
         "hasResults" bool
         :param studies_json_as_list:
         """
-        for element in studies_json_as_list:
-            if element.get("protocolSection"):
-                self.protocolSection = element.get("protocolSection")
-            if element.get("derivedSection"):
-                self.protocolSection = element.get("derivedSection")
-            if element.get("hasResults"):
-                self.protocolSection = element.get("hasResults")
+        self.protocolSection = protocolSection
+        self.derivedSection = derivedSection
+        self.hasResults = hasResults
 
 
-# class ProtocolSection:
-#     identificationModule: dict
-#     statusModule: dict
-#     sponsorCollaboratorsModule: dict
-#     descriptionModule: dict
-#     conditionsModule: dict
-#     designModule: dict
-#     armsInterventionsModule: dict
-#     outcomesModule: dict
-#     eligibilityModule: dict
-#     contactsLocationsModule: dict
-#
-#     def __init__(self, protocol_section_json_as_dict: dict):
-#         for element in protocol_section_json_as_dict:
-#             if element.get('identificationModule'):
-#                 self.identificationModule = element.get('identificationModule')
+class ProtocolSection:
+    identificationModule: object
+    statusModule: object
+    sponsorCollaboratorsModule: object
+    descriptionModule: object
+    conditionsModule: object
+    designModule: object
+    armsInterventionsModule: object
+    outcomesModule: object
+    eligibilityModule: object
+    contactsLocationsModule: object
+
+    def __init__(self, protocol_section_json_as_dict: dict):
+        # for element in protocol_section_json_as_dict:
+        if protocol_section_json_as_dict.get('identificationModule'):
+            self.identificationModule = IdentificationModule(protocol_section_json_as_dict.get('identificationModule'))
+            """
+            activate next pages when Classes are ready 
+            """
+        # if protocol_section_json_as_dict.get('statusModule'):
+        #     self.statusModule = StatusModule(protocol_section_json_as_dict.get('statusModule'))
+        # if protocol_section_json_as_dict.get('sponsorCollaboratorsModule'):
+        #     self.sponsorCollaboratorsModule = SponsorCollaboratorsModule(
+        #         protocol_section_json_as_dict.get('sponsorCollaboratorsModule'))
+        # if protocol_section_json_as_dict.get('descriptionModule'):
+        #     self.descriptionModule = DescriptionModule(protocol_section_json_as_dict.get('descriptionModule'))
+        # if protocol_section_json_as_dict.get('conditionsModule'):
+        #     self.conditionsModule = ConditionsModule(protocol_section_json_as_dict.get('conditionsModule'))
+        # if protocol_section_json_as_dict.get('designModule'):
+        #     self.designModule = DesignModule(protocol_section_json_as_dict.get('designModule'))
+        # if protocol_section_json_as_dict.get('armsInterventionsModule'):
+        #     self.armsInterventionsModule = ArmsInterventionsModule(
+        #         protocol_section_json_as_dict.get('armsInterventionsModule'))
+        # if protocol_section_json_as_dict.get('outcomesModule'):
+        #     self.outcomesModule = OutcomesModule(protocol_section_json_as_dict.get('outcomesModule'))
+        # if protocol_section_json_as_dict.get('eligibilityModule'):
+        #     self.eligibilityModule = EligibilityModule(protocol_section_json_as_dict.get('eligibilityModule'))
+        # if protocol_section_json_as_dict.get('contactsLocationsModule'):
+        #     self.contactsLocationsModule = ContactsLocationsModule(
+        #         protocol_section_json_as_dict.get('contactsLocationsModule'))
+
+
+class IdentificationModule:
+    nctId: object
+    orgStudyIdInfo: object
+    secondaryIdInfos: object
+    organization: object
+    briefTitle: object
+    officialTitle: object
+
+    def __init__(self, inf_o):
+        self.obj = inf_o
 
 
 if __name__ == "__main__":
     s = Main("data.json")
-    #print(s.nextPageToken)
-    a = Studies(s.studies)
 
-   # pr_section = ProtocolSection(s.a.protocolSection)
-    #print(pr_section.identificationModule)
+    s.fill_studies()
+    print(s.list_studies[0])
+
+    # print(s.nextPageToken)
+    # a = Studies(s.studies)
+    # print(a.derivedSection)
+    # pr_section = ProtocolSection(s.a.protocolSection)
+    # print(pr_section.identificationModule)
     pass
